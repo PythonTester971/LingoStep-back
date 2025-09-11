39da618fad6b012288adadefc89b4ecfc9bc1ac4
Geoline <geoline.denisse@gmail.com>
2025-09-11 13:42:15 +0200

    ajout du champs obligatoire username formulaire inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,6 +17,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
@@ -26,6 +27,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('username')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
