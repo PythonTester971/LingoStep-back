8228cd02efd26dd384a9fbfca2da81e9d1cac0e7
Geoline <geoline.denisse@gmail.com>
2025-09-12 12:19:19 +0200

    création cours anglais course missions user template card

--- src/Controller/User/UserLanguageCourseController.php
+++ src/Controller/User/UserLanguageCourseController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller\User;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\MissionRepository;
+use App\Repository\LanguageCourseRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserLanguageCourseController extends AbstractController
 {
-    #[Route('/user/language/course', name: 'app_user_language_course')]
-    public function index(): Response
+    #[Route('/languages_course/{id}/missions', name: 'language_course_missions')]
+    public function missions(int $id, LanguageCourseRepository $languageCourseRepository): Response
     {
-        return $this->render('user_language_course/index.html.twig', [
-            'controller_name' => 'UserLanguageCourseController',
+
+        $languageCourse = $languageCourseRepository->find($id);
+
+        $missions = $languageCourse->getMissions();
+
+        return $this->render('mission_list/index.html.twig', [
+            'languageCourse' => $languageCourse,
+            'missions' => $missions,
         ]);
     }
 }
