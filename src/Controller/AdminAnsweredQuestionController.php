83cc4af9257f197856d492b34804285ac2b78d18
Geoline <geoline.denisse@gmail.com>
2025-09-12 14:14:55 +0200

    création answered question controller pour voir liste

---
+++ src/Controller/AdminAnsweredQuestionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminAnsweredQuestionController extends AbstractController
+{
+    #[Route('/admin/answered/question', name: 'app_admin_answered_question')]
+    public function index(): Response
+    {
+        return $this->render('admin_answered_question/index.html.twig', [
+            'controller_name' => 'AdminAnsweredQuestionController',
+        ]);
+    }
+}
