fcf116e9c3a3e12477f9cf319eb2674022ac5cac
Geoline <geoline.denisse@gmail.com>
2025-09-15 14:13:11 +0200

    correction quiz récupération option sélectionnées

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -6,6 +6,7 @@ use App\Form\QuizType;
 use App\Entity\AnsweredQuestion;
 use App\Repository\MissionRepository;
 use App\Repository\QuestionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,7 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class QuizController extends AbstractController
 {
     #[Route('/quiz/mission/{mission_id}/question/{question_id}', name: 'app_quiz')]
-    public function index($mission_id, $question_id, MissionRepository $missionRepository, QuestionRepository $questionRepository, Request $request): Response
+    public function index($mission_id, $question_id, MissionRepository $missionRepository, QuestionRepository $questionRepository, Request $request, EntityManagerInterface $em): Response
     {
         $mission = $missionRepository->find($mission_id);
         $question = $questionRepository->find($question_id);
@@ -22,6 +23,7 @@ final class QuizController extends AbstractController
         $form = $this->createForm(QuizType::class, null, [
             'question' => $question,
         ]);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -31,16 +33,17 @@ final class QuizController extends AbstractController
             $answeredQuestion->setUser($this->getUser());
             $answeredQuestion->setMission($mission);
             $answeredQuestion->setQuestion($question);
-            $answeredQuestion->setOptione($data['option']);
+            $answeredQuestion->setOptione($data['question']);
 
-
-            // dd($data);
+            $em->persist($answeredQuestion);
+            $em->flush();
         }
 
 
         return $this->render('quiz/index.html.twig', [
-            'controller_name' => 'QuizController',
             'form' => $form->createView(),
+            'mission' => $mission,
+            'question' => $question,
         ]);
     }
 }
--- src/Form/QuizType.php
+++ src/Form/QuizType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Option;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Repository\OptionRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class QuizType extends AbstractType
@@ -20,6 +21,11 @@ class QuizType extends AbstractType
                 'choice_label' => 'label',
                 'expanded' => true,
                 'multiple' => false,
+                'query_builder' => function (OptionRepository $er) use ($options) {
+                    return $er->createQueryBuilder('o')
+                        ->where('o.question = :question')
+                        ->setParameter('question', $options['question']);
+                }
             ]);
     }
 
