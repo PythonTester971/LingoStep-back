4444a737d81483551d14f1147c48080684d11f21
Geoline <geoline.denisse@gmail.com>
2025-09-26 10:08:30 +0200

    home correction after merge et creation fichiers pour Contact

---
+++ src/Form/ContactType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email_adress')
+            ->add('name')
+            ->add('subject')
+            ->add('message_content')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
